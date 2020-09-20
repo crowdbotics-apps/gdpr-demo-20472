183898a59ef5289554ea717db62a68f51343b683
Crowdbotics <team@crowdbotics.com>
2020-09-20 06:52:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gdpr_demo_20472/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gdpr_demo_20472;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "gdpr_demo_20472";
+    }
+}
