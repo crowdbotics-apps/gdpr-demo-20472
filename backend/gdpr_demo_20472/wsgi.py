df6ebc4334f15ad805801617a0c4b6bcaa91fd45
Crowdbotics <team@crowdbotics.com>
2021-01-07 13:27:55 +0000

    Update models

--- backend/gdpr_demo_20472/wsgi.py
+++ backend/gdpr_demo_20472/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gdpr_demo_20472.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gdpr_demo_20472.settings")
 
 application = get_wsgi_application()
